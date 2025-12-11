81c56b31b9e381e84f9c6fd3eb605c790b27294d
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-12-11 11:32:36 -0500

    fix 1

--- submission.cpp
+++ submission.cpp
@@ -9,27 +9,11 @@
 
 #include <vector>
 #include <iostream>
+using namespace std;
 
-std::vector<int> append(std::vector<int> a, std::vector<int> b){
+vector<int> append(vector<int> a, vector<int> b){
     for(int i = 0; i < b.size(); i++){
         a.push_back(b[i]);
     }
     return a;
-}
-
-int main(){
-    std::vector a = {1, 2, 3, 4};
-    for(int i = 0; i < a.size(); i++){
-        std::cout << a[i] << std::endl;
-    }
-    std::cout << std::endl;
-    std::vector b = {5, 6, 7, 8};
-    for(int i = 0; i < b.size(); i++){
-        std::cout << b[i] << std::endl;
-    }
-    std::cout << std::endl;
-    std::vector c = append(a,b);
-    for(int i = 0; i < c.size(); i++){
-        std::cout << c[i] << std::endl;
-    }
 }
